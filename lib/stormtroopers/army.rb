7f3a92cecc551de5a1646cdc6aad67b10c93c4fc
Mark Kremer <mark@without-brains.net>
2012-11-06 20:13:42 +0100

    Allow us to specify the class directly too

--- lib/stormtroopers/army.rb
+++ lib/stormtroopers/army.rb
@@ -3,9 +3,16 @@ module Stormtroopers
     attr_reader :factory, :threads, :max_threads
 
     def initialize(config)
-      @factory = "stormtroopers/#{config[:factory].delete(:type)}_factory".camelize.constantize.new(config[:factory])
+      @factory = factory_class(config).new(config[:factory])
       @max_threads = config[:max_threads] || 1
       @threads = []
+    end
+
+    def factory_class(config)
+      raise ArgumentError, "Factory class or type must be defined" if config[:factory][:class].blank? && config[:factory][:type].blank?
+      class_name ||= config[:factory].delete(:class)
+      class_name ||= "stormtroopers/#{config[:factory].delete(:type)}_factory".camelize
+      class_name.constantize
     end
 
     def manage
