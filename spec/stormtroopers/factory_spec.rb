798bb95fe2182ecafca6bec5c3306f80d67a8ea8
Mark Kremer <mark@without-brains.net>
2012-11-13 07:14:01 +0100

    Adds specs for Factory

---
+++ spec/stormtroopers/factory_spec.rb
@@ -0,0 +1,28 @@
+require "stormtroopers"
+
+describe Stormtroopers::Factory do
+  describe "#options" do
+    it "exposes the options passed in during #initialize" do
+      options = {key1: "value1", key2: "value2"}
+      factory = Stormtroopers::Factory.new(options)
+      factory.options.should equal(options)
+    end
+  end
+
+  describe "#produce" do
+    it "is not implemented" do
+      factory = Stormtroopers::Factory.new
+      expect { factory.produce }.to raise_error(NotImplementedError)
+    end
+  end
+
+  describe "#logger" do
+    it "uses the Stormtroopers::Manager#logger" do
+      stub_const("Stormtroopers::Manager", Class.new)
+      logger = stub
+      Stormtroopers::Manager.stub(:logger).and_return(logger)
+      factory = Stormtroopers::Factory.new
+      factory.logger.should equal(logger)
+    end
+  end
+end
